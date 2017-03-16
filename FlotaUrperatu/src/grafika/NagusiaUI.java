87cc5c42a3eb53d77cf9e64cf0728f2e04658fa4
Andrea <Andrea@10.109.224.43>
2017-03-16 10:15:24 +0100

    nagusia

--- FlotaUrperatu/src/grafika/NagusiaUI.java
+++ FlotaUrperatu/src/grafika/NagusiaUI.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
