22b7cef578d47d4407e2697af5724828e11f39e4
Goizane <goizanealonso95@gmail.com>
2017-05-03 20:42:38 +0200

    JUnit

--- FlotaUrperatu/src/junit/HondoratuaTest.java
+++ FlotaUrperatu/src/junit/HondoratuaTest.java
@@ -1,12 +1,18 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Hondoratua;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class HondoratuaTest {
+	
+	Hondoratua hondoratua1 = new Hondoratua();
+	Hondoratua hondoratua2 = new Hondoratua();
+	Hondoratua hondoratua3 = new Hondoratua();
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,11 +24,30 @@ public class HondoratuaTest {
 
 	@Test
 	public void testMinEgin() {
-		fail("Not yet implemented");
+		assertNotNull(hondoratua1);
+		assertNotNull(hondoratua2);
+		assertNotNull(hondoratua3);
+		
+		assertEquals(hondoratua1.minEgin(), 0);
+		assertNotEquals(hondoratua1.minEgin(), 3);
+		assertNotEquals(hondoratua1.minEgin(), 100);
+		assertNotEquals(hondoratua1.minEgin(), 1);
+		
+		assertEquals(hondoratua2.minEgin(), 0);
+		assertNotEquals(hondoratua2.minEgin(), 9);
+		assertNotEquals(hondoratua2.minEgin(), 400);
+		assertNotEquals(hondoratua2.minEgin(), 2);
+		
+		assertEquals(hondoratua3.minEgin(), 0);
+		assertNotEquals(hondoratua3.minEgin(), 68);
+		assertNotEquals(hondoratua3.minEgin(), 40);
+		assertNotEquals(hondoratua3.minEgin(), 5);
 	}
 
 	@Test
 	public void testHondoratua() {
-		fail("Not yet implemented");
+		assertNotNull(hondoratua1);
+		assertNotNull(hondoratua2);
+		assertNotNull(hondoratua3);
 	}
 }
