5f9d4c8aa136b790a57fedce22236c8b3de84fa3
Goizane <goizanealonso95@gmail.com>
2017-05-03 20:14:59 +0200

    Proba gehiago

--- FlotaUrperatu/src/junit/EzkutatuaTest.java
+++ FlotaUrperatu/src/junit/EzkutatuaTest.java
@@ -1,12 +1,18 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Ezkutatua;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class EzkutatuaTest {
+	
+	Ezkutatua ezkutatua1 = new Ezkutatua();
+	Ezkutatua ezkutatua2 = new Ezkutatua();
+	Ezkutatua ezkutatua3 = new Ezkutatua();
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,16 +24,84 @@ public class EzkutatuaTest {
 
 	@Test
 	public void testMinEgin() {
-		fail("Not yet implemented");
+		assertTrue(ezkutatua1.getKontadorea()==2);
+		assertFalse(ezkutatua1.getKontadorea()==44);
+		ezkutatua1.minEgin();
+		assertTrue(ezkutatua1.getKontadorea()==1);
+		assertFalse(ezkutatua1.getKontadorea()==2);
+		ezkutatua1.minEgin();
+		assertTrue(ezkutatua1.getKontadorea()==0);
+		assertFalse(ezkutatua1.getKontadorea()==1);
+		
+		assertTrue(ezkutatua2.getKontadorea()==2);
+		assertFalse(ezkutatua2.getKontadorea()==333);
+		ezkutatua2.minEgin();
+		assertTrue(ezkutatua2.getKontadorea()==1);
+		assertFalse(ezkutatua2.getKontadorea()==2);
+		ezkutatua2.minEgin();
+		assertTrue(ezkutatua2.getKontadorea()==0);
+		assertFalse(ezkutatua2.getKontadorea()==1);
+		
+		assertTrue(ezkutatua3.getKontadorea()==2);
+		assertFalse(ezkutatua3.getKontadorea()==19);
+		ezkutatua3.minEgin();
+		assertTrue(ezkutatua3.getKontadorea()==1);
+		assertFalse(ezkutatua3.getKontadorea()==2);
+		ezkutatua3.minEgin();
+		assertTrue(ezkutatua3.getKontadorea()==0);
+		assertFalse(ezkutatua3.getKontadorea()==1);
 	}
 
 	@Test
 	public void testEzkutatua() {
-		fail("Not yet implemented");
+		assertNotNull(ezkutatua1);
+		assertNotNull(ezkutatua2);
+		assertNotNull(ezkutatua3);
+		
+		assertTrue(ezkutatua1.getKontadorea()==2);
+		assertTrue(ezkutatua2.getKontadorea()==2);
+		assertTrue(ezkutatua3.getKontadorea()==2);
 	}
 
 	@Test
 	public void testGetKontadorea() {
-		fail("Not yet implemented");
+		assertNotNull(ezkutatua1);
+		assertNotNull(ezkutatua2);
+		assertNotNull(ezkutatua3);
+		
+		assertNotNull(ezkutatua1.getKontadorea());
+		assertNotNull(ezkutatua2.getKontadorea());
+		assertNotNull(ezkutatua3.getKontadorea());
+		
+		assertTrue(ezkutatua1.getKontadorea()==2);
+		assertTrue(ezkutatua2.getKontadorea()==2);
+		assertTrue(ezkutatua3.getKontadorea()==2);
+		
+		assertTrue(ezkutatua1.getKontadorea()==2);
+		assertFalse(ezkutatua1.getKontadorea()==44);
+		ezkutatua1.minEgin();
+		assertTrue(ezkutatua1.getKontadorea()==1);
+		assertFalse(ezkutatua1.getKontadorea()==2);
+		ezkutatua1.minEgin();
+		assertTrue(ezkutatua1.getKontadorea()==0);
+		assertFalse(ezkutatua1.getKontadorea()==1);
+		
+		assertTrue(ezkutatua2.getKontadorea()==2);
+		assertFalse(ezkutatua2.getKontadorea()==333);
+		ezkutatua2.minEgin();
+		assertTrue(ezkutatua2.getKontadorea()==1);
+		assertFalse(ezkutatua2.getKontadorea()==2);
+		ezkutatua2.minEgin();
+		assertTrue(ezkutatua2.getKontadorea()==0);
+		assertFalse(ezkutatua2.getKontadorea()==1);
+		
+		assertTrue(ezkutatua3.getKontadorea()==2);
+		assertFalse(ezkutatua3.getKontadorea()==19);
+		ezkutatua3.minEgin();
+		assertTrue(ezkutatua3.getKontadorea()==1);
+		assertFalse(ezkutatua3.getKontadorea()==2);
+		ezkutatua3.minEgin();
+		assertTrue(ezkutatua3.getKontadorea()==0);
+		assertFalse(ezkutatua3.getKontadorea()==1);
 	}
 }
--- FlotaUrperatu/src/junit/EzkutuaTest.java
+++ FlotaUrperatu/src/junit/EzkutuaTest.java
@@ -1,12 +1,19 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Ezkutua;
+import logika.Ontzia;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class EzkutuaTest {
+	
+	Ezkutua ezkutua1 = new Ezkutua(5);
+	Ezkutua ezkutua2 = new Ezkutua(10);
+	Ezkutua ezkutua3 = new Ezkutua(15);
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,21 +25,109 @@ public class EzkutuaTest {
 
 	@Test
 	public void testEzkutua() {
-		fail("Not yet implemented");
+		assertNotNull(ezkutua1);
+		assertNotNull(ezkutua2);
+		assertNotNull(ezkutua3);
+		
+		assertTrue(ezkutua1.getKantitatea()==5);
+		assertFalse(ezkutua1.getKantitatea()==155);
+		
+		assertTrue(ezkutua2.getKantitatea()==10);
+		assertFalse(ezkutua2.getKantitatea()==777);
+		
+		assertTrue(ezkutua3.getKantitatea()==15);
+		assertFalse(ezkutua3.getKantitatea()==100);
 	}
 
 	@Test
 	public void testOntziaEzkututatu() {
-		fail("Not yet implemented");
+		Ontzia ontzia1 = new Ontzia("Itsaspeko");
+		String egoeraNormala = ontzia1.getEgora();
+		ezkutua1.ontziaEzkututatu(ontzia1);
+		String egoeraEzkutatua = ontzia1.getEgora();
+		assertNotEquals(egoeraEzkutatua, egoeraNormala);
+		
+		Ontzia ontzia2 = new Ontzia("Itsaspeko");
+		String egoeraNormala2 = ontzia2.getEgora();
+		ezkutua2.ontziaEzkututatu(ontzia2);
+		String egoeraEzkutatua2 = ontzia2.getEgora();
+		assertNotEquals(egoeraEzkutatua2, egoeraNormala2);
+		
+		Ontzia ontzia3 = new Ontzia("Itsaspeko");
+		String egoeraNormala3 = ontzia3.getEgora();
+		ezkutua3.ontziaEzkututatu(ontzia3);
+		String egoeraEzkutatua3 = ontzia3.getEgora();
+		assertNotEquals(egoeraEzkutatua3, egoeraNormala3);
 	}
 
 	@Test
 	public void testEzkutuaErabili() {
-		fail("Not yet implemented");
+		assertNotNull(ezkutua1);
+		assertNotNull(ezkutua2);
+		assertNotNull(ezkutua3);
+		
+		assertTrue(ezkutua1.getKantitatea()==5);
+		assertFalse(ezkutua1.getKantitatea()==155);
+		
+		assertTrue(ezkutua2.getKantitatea()==10);
+		assertFalse(ezkutua2.getKantitatea()==777);
+		
+		assertTrue(ezkutua3.getKantitatea()==15);
+		assertFalse(ezkutua3.getKantitatea()==100);
+		
+		ezkutua1.ezkutuaErabili();
+		assertTrue(ezkutua1.getKantitatea()==4);
+		assertFalse(ezkutua1.getKantitatea()==5);
+		ezkutua1.ezkutuaErabili();
+		assertTrue(ezkutua1.getKantitatea()==3);
+		assertFalse(ezkutua1.getKantitatea()==4);
+		ezkutua1.ezkutuaErabili();
+		assertTrue(ezkutua1.getKantitatea()==2);
+		assertFalse(ezkutua1.getKantitatea()==3);
+		ezkutua1.ezkutuaErabili();
+		assertTrue(ezkutua1.getKantitatea()==1);
+		assertFalse(ezkutua1.getKantitatea()==2);
+		
+		ezkutua2.ezkutuaErabili();
+		assertTrue(ezkutua2.getKantitatea()==9);
+		assertFalse(ezkutua2.getKantitatea()==10);
+		ezkutua2.ezkutuaErabili();
+		assertTrue(ezkutua2.getKantitatea()==8);
+		assertFalse(ezkutua2.getKantitatea()==9);
+		ezkutua2.ezkutuaErabili();
+		assertTrue(ezkutua2.getKantitatea()==7);
+		assertFalse(ezkutua2.getKantitatea()==8);
+		ezkutua2.ezkutuaErabili();
+		assertTrue(ezkutua2.getKantitatea()==6);
+		assertFalse(ezkutua2.getKantitatea()==7);
+		
+		ezkutua3.ezkutuaErabili();
+		assertTrue(ezkutua3.getKantitatea()==14);
+		assertFalse(ezkutua3.getKantitatea()==15);
+		ezkutua3.ezkutuaErabili();
+		assertTrue(ezkutua3.getKantitatea()==13);
+		assertFalse(ezkutua3.getKantitatea()==14);
+		ezkutua3.ezkutuaErabili();
+		assertTrue(ezkutua3.getKantitatea()==12);
+		assertFalse(ezkutua3.getKantitatea()==13);
+		ezkutua3.ezkutuaErabili();
+		assertTrue(ezkutua3.getKantitatea()==11);
+		assertFalse(ezkutua3.getKantitatea()==12);
 	}
 
 	@Test
 	public void testGetKantitatea() {
-		fail("Not yet implemented");
+		assertNotNull(ezkutua1);
+		assertNotNull(ezkutua2);
+		assertNotNull(ezkutua3);
+		
+		assertTrue(ezkutua1.getKantitatea()==5);
+		assertFalse(ezkutua1.getKantitatea()==155);
+		
+		assertTrue(ezkutua2.getKantitatea()==10);
+		assertFalse(ezkutua2.getKantitatea()==777);
+		
+		assertTrue(ezkutua3.getKantitatea()==15);
+		assertFalse(ezkutua3.getKantitatea()==100);
 	}
 }
