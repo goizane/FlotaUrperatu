719fb60cee6ceed3cafc744bb1b2e9e45595a5e3
Goizane <goizanealonso95@gmail.com>
2017-05-07 01:07:22 +0200

    Normala eta Ukitua testeatuta!

--- FlotaUrperatu/src/junit/NormalaTest.java
+++ FlotaUrperatu/src/junit/NormalaTest.java
@@ -1,12 +1,18 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Normala;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class NormalaTest {
+	
+	Normala normala1 = new Normala();
+	Normala normala2 = new Normala();
+	Normala normala3 = new Normala();
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,11 +24,24 @@ public class NormalaTest {
 
 	@Test
 	public void testMinEgin() {
-		fail("Not yet implemented");
+		assertNotNull(normala1);
+		assertNotNull(normala2);
+		assertNotNull(normala3);
+		
+		assertEquals(normala1.minEgin(), 0);
+		assertNotEquals(normala1.minEgin(), 1);
+		
+		assertEquals(normala2.minEgin(), 0);
+		assertNotEquals(normala2.minEgin(), 1);
+		
+		assertEquals(normala3.minEgin(), 0);
+		assertNotEquals(normala3.minEgin(), 1);
 	}
 
 	@Test
 	public void testNormala() {
-		fail("Not yet implemented");
+		assertNotNull(normala1);
+		assertNotNull(normala2);
+		assertNotNull(normala3);
 	}
 }
--- FlotaUrperatu/src/junit/UkituaTest.java
+++ FlotaUrperatu/src/junit/UkituaTest.java
@@ -1,12 +1,18 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Ukitua;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class UkituaTest {
+	
+	Ukitua ukitua1;
+	Ukitua ukitua2;
+	Ukitua ukitua3;
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,11 +24,34 @@ public class UkituaTest {
 
 	@Test
 	public void testMinEgin() {
-		fail("Not yet implemented");
+		ukitua1 = new Ukitua(2);
+		ukitua2 = new Ukitua(3);
+		ukitua3 = new Ukitua(4);
+		
+		assertEquals(ukitua1.minEgin(), 1);
+		assertEquals(ukitua1.minEgin(), 2);
+		assertEquals(ukitua1.minEgin(), 3);
+		assertEquals(ukitua1.minEgin(), 4);
+		
+		assertEquals(ukitua2.minEgin(), 1);
+		assertEquals(ukitua2.minEgin(), 2);
+		assertEquals(ukitua2.minEgin(), 3);
+		assertEquals(ukitua2.minEgin(), 4);
+		
+		assertEquals(ukitua3.minEgin(), 1);
+		assertEquals(ukitua3.minEgin(), 2);
+		assertEquals(ukitua3.minEgin(), 3);
+		assertEquals(ukitua3.minEgin(), 4);
 	}
 
 	@Test
 	public void testUkitua() {
-		fail("Not yet implemented");
+		ukitua1 = new Ukitua(2);
+		ukitua2 = new Ukitua(3);
+		ukitua3 = new Ukitua(4);
+		
+		assertNotNull(ukitua1);
+		assertNotNull(ukitua2);
+		assertNotNull(ukitua3);
 	}
 }
