48c26eddcf86d32bcdc03ebf5c5413c6e7141a48
Goizane <goizanealonso95@gmail.com>
2017-05-09 22:15:45 +0200

    Radar eta Taula testeatuta!

--- FlotaUrperatu/src/junit/RadarTest.java
+++ FlotaUrperatu/src/junit/RadarTest.java
@@ -1,12 +1,18 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Radar;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class RadarTest {
+	
+	Radar radar1;
+	Radar radar2;
+	Radar radar3;
 
 	@Before
 	public void setUp() throws Exception {
@@ -14,30 +20,113 @@ public class RadarTest {
 
 	@After
 	public void tearDown() throws Exception {
+		radar1 = null;
+		radar2 = null;
+		radar3 = null;
 	}
 
 	@Test
 	public void testRadar() {
-		fail("Not yet implemented");
+		radar1 = new Radar(5);
+		radar2 = new Radar(10);
+		radar3 = new Radar(15);
+		
+		assertNotNull(radar1);
+		assertNotNull(radar2);
+		assertNotNull(radar3);
+		
+		assertEquals(radar1.getKant(), 5);
+		assertEquals(radar2.getKant(), 10);
+		assertEquals(radar3.getKant(), 15);
 	}
 
 	@Test
 	public void testGetKant() {
-		fail("Not yet implemented");
+		radar1 = new Radar(5);
+		radar2 = new Radar(10);
+		radar3 = new Radar(15);
+		
+		assertNotNull(radar1);
+		assertNotNull(radar2);
+		assertNotNull(radar3);
+		
+		assertEquals(radar1.getKant(), 5);
+		assertNotEquals(radar1.getKant(), 6);
+		assertNotEquals(radar1.getKant(), 100);
+		
+		assertEquals(radar2.getKant(), 10);
+		assertNotEquals(radar2.getKant(), 33);
+		assertNotEquals(radar2.getKant(), 100);
+		
+		assertEquals(radar3.getKant(), 15);
+		assertNotEquals(radar3.getKant(), 25);
+		assertNotEquals(radar3.getKant(), 100);
 	}
 
 	@Test
 	public void testPosizioa() {
-		fail("Not yet implemented");
+		radar1 = new Radar(5);
+		radar2 = new Radar(10);
+		radar3 = new Radar(15);
+		
+		assertNotNull(radar1);
+		assertNotNull(radar2);
+		assertNotNull(radar3);
+		
+		assertNotNull(radar1.posizioa());
+		assertNotNull(radar2.posizioa());
+		assertNotNull(radar3.posizioa());
 	}
 
 	@Test
 	public void testPosAldatu() {
-		fail("Not yet implemented");
+		radar1 = new Radar(5);
+		radar2 = new Radar(10);
+		radar3 = new Radar(15);
+		
+		assertNotNull(radar1);
+		assertNotNull(radar2);
+		assertNotNull(radar3);
+		
+		assertNotNull(radar1.posizioa());
+		radar1.posAldatu(3, 3);
+		assertNotNull(radar1.posizioa());
+		
+		assertNotNull(radar2.posizioa());
+		radar2.posAldatu(3, 4);
+		assertNotNull(radar2.posizioa());
+		
+		assertNotNull(radar3.posizioa());
+		radar3.posAldatu(5, 6);
+		assertNotNull(radar3.posizioa());
 	}
 
 	@Test
 	public void testErabili() {
-		fail("Not yet implemented");
+		radar1 = new Radar(5);
+		radar2 = new Radar(10);
+		radar3 = new Radar(15);
+		
+		assertNotNull(radar1);
+		assertNotNull(radar2);
+		assertNotNull(radar3);
+		
+		assertEquals(radar1.getKant(), 5);
+		assertNotEquals(radar1.getKant(), 4);
+		radar1.erabili();
+		assertNotEquals(radar1.getKant(), 5);
+		assertEquals(radar1.getKant(), 4);
+		
+		assertEquals(radar2.getKant(), 10);
+		assertNotEquals(radar2.getKant(), 9);
+		radar2.erabili();
+		assertNotEquals(radar2.getKant(), 10);
+		assertEquals(radar2.getKant(), 9);
+		
+		assertEquals(radar3.getKant(), 15);
+		assertNotEquals(radar3.getKant(), 14);
+		radar3.erabili();
+		assertNotEquals(radar3.getKant(), 15);
+		assertEquals(radar3.getKant(), 14);
 	}
 }
--- FlotaUrperatu/src/junit/TaulaTest.java
+++ FlotaUrperatu/src/junit/TaulaTest.java
@@ -1,12 +1,20 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import logika.Laukia;
+import logika.Ontzia;
+import logika.Taula;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class TaulaTest {
+	
+	Taula taula1;
+	Taula taula2;
+	Taula taula3;
 
 	@Before
 	public void setUp() throws Exception {
@@ -14,45 +22,183 @@ public class TaulaTest {
 
 	@After
 	public void tearDown() throws Exception {
+		taula1 = null;
+		taula2 = null;
+		taula3 = null;
 	}
 
 	@Test
 	public void testTaula() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
 	}
 
 	@Test
 	public void testJarDaiteke() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		assertTrue(taula1.jarDaiteke(3, 4, 5, 'h'));
+		assertTrue(taula1.jarDaiteke(2, 6, 7, 'h'));
+		assertFalse(taula1.jarDaiteke(3, 9, 9, 'h'));
+		assertFalse(taula1.jarDaiteke(2, 9, 9, 'h'));
+		assertFalse(taula1.jarDaiteke(3, 8, 8, 'h'));
 	}
 
 	@Test
 	public void testOntziaKokatu() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		Ontzia ontzia1 = new Ontzia("Itsaspeko");
+		Ontzia ontzia2 = new Ontzia("Itsaspeko");
+		Ontzia ontzia3 = new Ontzia("Itsaspeko");
+		
+		assertTrue(taula1.jarDaiteke(3, 3, 3, 'h'));
+		taula1.ontziaKokatu(3, 3, 3, 'h', ontzia1);
+		assertFalse(taula1.jarDaiteke(3, 3, 3, 'h'));
+		
+		assertTrue(taula2.jarDaiteke(2, 6, 6, 'h'));
+		taula2.ontziaKokatu(2, 6, 6, 'h', ontzia2);
+		assertFalse(taula2.jarDaiteke(2, 6, 6, 'h'));
+		
+		assertTrue(taula3.jarDaiteke(3, 5, 5, 'h'));
+		taula3.ontziaKokatu(3, 5, 5, 'h', ontzia3);
+		assertFalse(taula3.jarDaiteke(3, 5, 5, 'h'));
 	}
 
 	@Test
 	public void testTamaina() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		assertEquals(taula1.tamaina(), 10);
+		assertNotEquals(taula1.tamaina(), 9);
+		assertEquals(taula2.tamaina(), 10);
+		assertNotEquals(taula2.tamaina(), 9);
+		assertEquals(taula3.tamaina(), 10);
+		assertNotEquals(taula3.tamaina(), 9);
 	}
 
 	@Test
 	public void testGetTaula() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		Laukia[][] laukia1 = taula1.getTaula();
+		assertNotNull(laukia1);
+		Laukia[][] laukia2 = taula2.getTaula();
+		assertNotNull(laukia2);
+		Laukia[][] laukia3 = taula3.getTaula();
+		assertNotNull(laukia3);
 	}
 
 	@Test
 	public void testInprimatu() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		taula1.inprimatu();
+		taula2.inprimatu();
+		taula3.inprimatu();
 	}
 
 	@Test
 	public void testOntziaDa() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		Ontzia ontzia1 = new Ontzia("Itsaspeko");
+		Ontzia ontzia2 = new Ontzia("Itsaspeko");
+		Ontzia ontzia3 = new Ontzia("Itsaspeko");
+		
+		assertTrue(taula1.jarDaiteke(3, 3, 3, 'h'));
+		taula1.ontziaKokatu(3, 3, 3, 'h', ontzia1);
+		assertFalse(taula1.jarDaiteke(3, 3, 3, 'h'));
+		assertTrue(taula1.ontziaDa(3, 3));
+		assertFalse(taula1.ontziaDa(1, 1));
+		
+		assertTrue(taula2.jarDaiteke(2, 6, 6, 'h'));
+		taula2.ontziaKokatu(2, 6, 6, 'h', ontzia2);
+		assertFalse(taula2.jarDaiteke(2, 6, 6, 'h'));
+		assertTrue(taula2.ontziaDa(6, 6));
+		assertFalse(taula2.ontziaDa(3, 3));
+		
+		assertTrue(taula3.jarDaiteke(3, 5, 5, 'h'));
+		taula3.ontziaKokatu(3, 5, 5, 'h', ontzia3);
+		assertFalse(taula3.jarDaiteke(3, 5, 5, 'h'));
+		assertTrue(taula3.ontziaDa(5, 5));
+		assertFalse(taula3.ontziaDa(1, 1));
 	}
 
 	@Test
 	public void testHondoratua() {
-		fail("Not yet implemented");
+		taula1 = new Taula();
+		taula2 = new Taula();
+		taula3 = new Taula();
+		
+		assertNotNull(taula1);
+		assertNotNull(taula2);
+		assertNotNull(taula3);
+		
+		Ontzia ontzia1 = new Ontzia("Itsaspeko");
+		Ontzia ontzia2 = new Ontzia("Itsaspeko");
+		Ontzia ontzia3 = new Ontzia("Itsaspeko");
+		
+		assertTrue(taula1.jarDaiteke(3, 3, 3, 'h'));
+		taula1.ontziaKokatu(3, 3, 3, 'h', ontzia1);
+		assertFalse(taula1.jarDaiteke(3, 3, 3, 'h'));
+		assertTrue(taula1.ontziaDa(3, 3));
+		assertFalse(taula1.ontziaDa(1, 1));
+		assertFalse(taula1.hondoratua(3, 3));
+		
+		assertTrue(taula2.jarDaiteke(2, 6, 6, 'h'));
+		taula2.ontziaKokatu(2, 6, 6, 'h', ontzia2);
+		assertFalse(taula2.jarDaiteke(2, 6, 6, 'h'));
+		assertTrue(taula2.ontziaDa(6, 6));
+		assertFalse(taula2.ontziaDa(3, 3));
+		assertFalse(taula2.hondoratua(6, 6));
+		
+		assertTrue(taula3.jarDaiteke(3, 5, 5, 'h'));
+		taula3.ontziaKokatu(3, 5, 5, 'h', ontzia3);
+		assertFalse(taula3.jarDaiteke(3, 5, 5, 'h'));
+		assertTrue(taula3.ontziaDa(5, 5));
+		assertFalse(taula3.ontziaDa(1, 1));
+		assertFalse(taula3.hondoratua(5, 5));
 	}
 }
