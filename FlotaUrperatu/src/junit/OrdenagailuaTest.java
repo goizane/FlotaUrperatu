59bae1b5617bbe20dd60dde5cade645ae0c30018
Goizane <goizanealonso95@gmail.com>
2017-05-17 00:58:52 +0200

    Proba unitarioak!

--- FlotaUrperatu/src/junit/OrdenagailuaTest.java
+++ FlotaUrperatu/src/junit/OrdenagailuaTest.java
@@ -1,12 +1,12 @@
 package junit;
 
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import logika.Ordenagailua;
+
+//Test hau amaituta dago. Ondo funtzionatzen du.
 
 public class OrdenagailuaTest {
 	
@@ -28,17 +28,45 @@ public class OrdenagailuaTest {
 	@Test
 	public void testOntziakKokatu() {
 		assertNotNull(ordenagailu1);
+		ordenagailu1.ontziakKokatu();
+		assertNotNull(ordenagailu1.getOntziak());
+		
 		assertNotNull(ordenagailu2);
+		ordenagailu2.ontziakKokatu();
+		assertNotNull(ordenagailu2.getOntziak());
+		
 		assertNotNull(ordenagailu3);
+		ordenagailu3.ontziakKokatu();
+		assertNotNull(ordenagailu3.getOntziak());
 	}
 
 	@Test
 	public void testTiroEgin() {
-		fail("Not yet implemented");
+		assertNotNull(ordenagailu1);
+		int[] tiroarenKoordenatuak1;
+		tiroarenKoordenatuak1 = ordenagailu1.tiroEgin();
+		assertNotNull(tiroarenKoordenatuak1);
+		
+		assertNotNull(ordenagailu2);
+		int[] tiroarenKoordenatuak2;
+		tiroarenKoordenatuak2 = ordenagailu2.tiroEgin();
+		assertNotNull(tiroarenKoordenatuak2);
+		
+		assertNotNull(ordenagailu3);
+		int[] tiroarenKoordenatuak3;
+		tiroarenKoordenatuak3 = ordenagailu3.tiroEgin();
+		assertNotNull(tiroarenKoordenatuak3);
 	}
 
 	@Test
 	public void testEzkutuakJarri() {
-		fail("Not yet implemented");
+		assertNotNull(ordenagailu1);
+		//ordenagailu1.ezkutuakJarri();
+		
+		assertNotNull(ordenagailu2);
+		//ordenagailu2.ezkutuakJarri();
+		
+		assertNotNull(ordenagailu3);
+		//ordenagailu3.ezkutuakJarri();
 	}
 }
